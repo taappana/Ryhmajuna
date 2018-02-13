f13e834829e0c06f0641e87cae62f2810b7d2e78
WaltteriBroder <waltteri.broder@hotmail.com>
2018-02-13 13:16:50 +0200

    Maini luotu

---
+++ src/fi/academy/Main.java
@@ -0,0 +1,14 @@
+package fi.academy;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Anna lähtöasema");
+        String lahtoasema = lukija.nextLine();
+
+
+    }
+}
